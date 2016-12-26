3e9b8c078b01d6df3fbd47fd055eb6588acae522
Andres Canal <andresyo990@gmail.com>
2016-12-26 10:24:48 -0300

    first commit

---
+++ Pods/Target Support Files/CocoaLumberjack/CocoaLumberjack-umbrella.h
@@ -0,0 +1,22 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "DDAbstractDatabaseLogger.h"
+#import "DDASLLogCapture.h"
+#import "DDASLLogger.h"
+#import "DDAssertMacros.h"
+#import "DDFileLogger.h"
+#import "DDLegacyMacros.h"
+#import "DDLog+LOGV.h"
+#import "DDLog.h"
+#import "DDLogMacros.h"
+#import "DDTTYLogger.h"
+#import "CocoaLumberjack.h"
+#import "DDContextFilterLogFormatter.h"
+#import "DDDispatchQueueLogFormatter.h"
+#import "DDMultiFormatter.h"
+
+FOUNDATION_EXPORT double CocoaLumberjackVersionNumber;
+FOUNDATION_EXPORT const unsigned char CocoaLumberjackVersionString[];
+
---
+++ Pods/Target Support Files/KissXML/KissXML-umbrella.h
@@ -0,0 +1,15 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "DDXMLElementAdditions.h"
+#import "NSString+DDXML.h"
+#import "DDXML.h"
+#import "DDXMLDocument.h"
+#import "DDXMLElement.h"
+#import "DDXMLNode.h"
+#import "KissXML.h"
+
+FOUNDATION_EXPORT double KissXMLVersionNumber;
+FOUNDATION_EXPORT const unsigned char KissXMLVersionString[];
+
